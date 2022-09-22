b440ed390e36b259aaad4daa3ad8206a17d1ddaf
alaWilliams <95369346+alaWilliams@users.noreply.github.com>
2022-09-22 09:00:11 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -8,43 +8,64 @@ console.log(iot);
 
 
 // creating an object that contains users id paired with their devices id
-let userDevicePaired = {}
+const userDevicePaired = {}
+
+
+function createUserDevicePair() {
 for (let i = 0; i < users.length; i++) {
   userDevicePaired[users[i].id] = [];
   for (let j = 0; j < mobile.length; j++) {
     if (users[i].id === mobile[j].user) {
       userDevicePaired[users[i].id].push(mobile[j].id);
-  }; }
-}
+      }; 
+    };
+  };
+};
+createUserDevicePair();
 
 //creating an object that contains users id paired with their iot id
-const obj2 = {};
+const iotPair = {};
+
+function createUserIotPair() {
 for (const user in userDevicePaired) {
-  obj2[user] = []
+  iotPair[user] = []
     for (let i = 0; i < iot.length; i++) {
-      userDevicePaired[user].forEach(element => element === iot[i].mobile ? obj2[user].push(iot[i].id) : undefined);
-      }
+      userDevicePaired[user].forEach(element => element === iot[i].mobile ? iotPair[user].push(iot[i].id) : undefined);
     }
-  
+  }
+};  
+createUserIotPair();
 
 //replacing user id with user name
-for (const user in obj2) {
+function userIdToUserName() {
+for (const user in iotPair) {
   for (let i = 0; i < users.length; i++) {
-    users[i].id === user ? obj2[users[i].name] = obj2[user] : undefined;
-  }
-  delete obj2[user]; 
-}
+    users[i].id === user ? iotPair[users[i].name] = iotPair[user] : undefined;
+    };
+  delete iotPair[user]; 
+  };
+};
+
+userIdToUserName();
+
+console.log(iotPair)
+
+//creating an object that contains merged arrays of same named users 
+const userIotPair = {};
 
 
-console.log(obj2)
-
-//creating an object 
-const obj3 = {};
-
-for (const uName in obj2) {
+for (const uName in iotPair) {
   const nameArr = uName.split(' ');
+  //console.log(obj2[uName])
+  //for (let i = 0; i < users.length; i++) {
+  //  console.log(obj2[uName] )
+  nameArr[0] === uName.split(' ')[0] ? userIotPair[nameArr[0]] = iotPair[uName] : undefined;
   for (let i = 0; i < users.length; i++) {
-   // nameArr[0] === uName[i].split(' ')[0] ? obj3[nameArr[0]] = obj2[user[j]] : undefined;
+  
   }
 }
-//missing steps: second loop so when the condition is met I add the value to obj3; count the length of the array to return the total number of iot assigned to users 
+  
+
+  //= [...obj2[uName], ...obj2[uName]]
+console.log(userIotPair)
+ 
